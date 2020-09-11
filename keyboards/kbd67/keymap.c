210eba93c559f51224741bfbed9b9988de47f831
Elias Renman <elias.renman00@gmail.com>
2020-09-11 09:44:35 +0200

    updated kbd67 layout

--- keyboards/kbd67/keymap.c
+++ keyboards/kbd67/keymap.c
@@ -15,9 +15,9 @@
  */
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FL 1
-#define _ALGR 2
+#define _MBL 0
+#define _BL 1
+#define _FL 2
 
 enum custom_keycodes {
     RALT_COM = SAFE_RANGE,
@@ -27,7 +27,8 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap (Base Layer) Default Layer
+  
+  /* Keymap (Mac Base Layer) Default Mac Compatibility Layer
    * ,----------------------------------------------------------------.
    * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
    * |----------------------------------------------------------------|
@@ -40,12 +41,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt |Win |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_BL] = LAYOUT_65_ansi(
-  KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC, \
-  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
-  KC_CLCK, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,\
-  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   RALT_COM, RALT_DOT,  RALT_SLSH, KC_RSFT, KC_UP,            KC_PGDN, \
-  KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                          KC_RALT, MO(_FL),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+  [_MBL] = LAYOUT_65_ansi(
+    KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC, \
+    KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
+    KC_CLCK, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,\
+    KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   RALT_COM, RALT_DOT,  RALT_SLSH, KC_RSFT, KC_UP,            KC_PGDN, \
+    KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                          KC_RALT, MO(_FL),   TT(_BL), KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+  /* Keymap (Base Layer) Default Layer
+   * ,----------------------------------------------------------------.
+   * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
+   * |----------------------------------------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
+   * |----------------------------------------------------------------|
+   * |  Clck |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
+   * |----------------------------------------------------------------|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
+   * |----------------------------------------------------------------|
+   * |Ctrl|Alt |Win |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
+   * `----------------------------------------------------------------'
+   */
+  [_BL] = LAYOUT_65_ansi(
+    KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC, \
+    KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
+    KC_CLCK, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,\
+    KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   RALT_COM, RALT_DOT,  RALT_SLSH, KC_RSFT, KC_UP,            KC_PGDN, \
+    KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                           MO(_FL), KC_RALT,   _______, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Keymap _FL1: Function Layer
